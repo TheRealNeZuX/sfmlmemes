a7ee85019a45919eeff598e467885f611d6e17dd
Leo <83018357+TheRealNeZuX@users.noreply.github.com>
2022-04-15 17:10:11 +0300

    Add files via upload

--- circles.cpp
+++ circles.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <chrono>
+#include <thread>
 
 int main(){
     sf::RenderWindow window(sf::VideoMode(1800, 720), "CIRCLES");
@@ -7,7 +9,12 @@ int main(){
     std::vector<sf::CircleShape> circles;
     while (window.isOpen()){
         for (int i = 0; i < circles.size(); ++i) {
-            circles[i].setRadius(circles[i].getRadius()-1);
+            if(circles[i].getRadius()==0){
+                circles.erase(circles.begin()+i);
+            }else{
+                circles[i].setRadius(circles[i].getRadius()-1);
+                circles[i].setPosition(circles[i].getPosition().x+1, circles[i].getPosition().y+1);
+            }
         }
         sf::Event event;
         while(window.pollEvent(event)){
@@ -15,8 +22,8 @@ int main(){
                 window.close();
             }
             if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-                circle.setRadius(20);
-                circle.setPosition(sf::Mouse::getPosition(window).x-20, sf::Mouse::getPosition(window).y-20);
+                circle.setRadius(40);
+                circle.setPosition(sf::Mouse::getPosition(window).x-40, sf::Mouse::getPosition(window).y-40);
                 circle.setFillColor(sf::Color(160, 180, 250));
                 circles.push_back(circle);
             }
@@ -26,6 +33,7 @@ int main(){
         for (int i = 0; i < circles.size(); ++i) {
             window.draw(circles[i]);
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(15));
         window.display();
     }
 }
